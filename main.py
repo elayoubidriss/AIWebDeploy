ce4a2ee9cb941c4bf2393681d48d23107727c225
elayoubidriss <drisselayoubi4@gmail.com>
2021-12-18 10:45:05 +0100

    First commit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from cnn_api import app
+import os
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    port = int(os.environ.get('PORT', 33507))
+    app.run(host='0.0.0.0', debug=True, port=port)
 
